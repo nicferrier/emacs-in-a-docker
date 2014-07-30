e7b0e5e3180378f6def533b26fc5ea00c632ab48
Nic Ferrier <nferrier@ferrier.me.uk>
2014-07-30 23:42:28 +0100

    docker build for emacs daemon

---
+++ emacsd/dot-emacs.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives (quote (("marmalade" . "http://marmalade-repo.org/packages/") ("gnu" . "http://elpa.gnu.org/packages/"))))
+ '(server-auth-key "e-%-6uuO2Q|4D%~;MS2c7-QOSjZ~/,LQtLteND*X5~lyfkpn5/|=yB/~e-iH7ggJ")
+ '(server-host "0.0.0.0" t)
+ '(server-port 6001 t)
+ '(server-use-tcp t t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
