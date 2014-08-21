1df6f8262652c16a77a39a09d578f897e9417e0e
Nic Ferrier <nferrier@ferrier.me.uk>
2014-08-21 21:41:36 +0100

    changes to emacsd to use unix socket via VOLUME

--- emacsd/dot-emacs.el
+++ emacsd/dot-emacs.el
@@ -1,13 +1,14 @@
+(setq server-socket-dir
+      (format "%s/emacs%d/sock"
+              (or (getenv "TMPDIR") "/tmp")
+              (user-uid)))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-archives (quote (("marmalade" . "http://marmalade-repo.org/packages/") ("gnu" . "http://elpa.gnu.org/packages/"))))
- '(server-auth-key "e-%-6uuO2Q|4D%~;MS2c7-QOSjZ~/,LQtLteND*X5~lyfkpn5/|=yB/~e-iH7ggJ")
- '(server-host "0.0.0.0" t)
- '(server-port 6001 t)
- '(server-use-tcp t t))
+ '(server-auth-dir))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
